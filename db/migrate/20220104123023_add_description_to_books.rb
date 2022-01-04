72bd953dfe5afb18bfa99d009dc6cd3977d570dc
shimonslit@gmail.com <shimonslit@gmail.com>
2022-01-04 13:50:23 +0100

    remove the duplicate column name image url

--- db/migrate/20220104123023_add_description_to_books.rb
+++ db/migrate/20220104123023_add_description_to_books.rb
@@ -1,7 +1,5 @@
 class AddDescriptionToBooks < ActiveRecord::Migration[6.1]
   def change
     add_column :books, :description, :string
-    add_column :books, :AddImageUrlToBooks, :string
-    add_column :books, :image_url, :string
   end
 end
---
+++ db/schema.rb
@@ -0,0 +1,62 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2022_01_04_123023) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.string "editor"
+    t.integer "year"
+    t.integer "pages"
+    t.string "genre"
+    t.string "format"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "condition"
+    t.string "description"
+    t.string "AddImageUrlToBooks"
+    t.string "image_url"
+  end
+
+  create_table "old_new_papers", force: :cascade do |t|
+    t.string "name"
+    t.integer "year"
+    t.string "date"
+    t.string "genre"
+    t.string "image_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "old_papers", force: :cascade do |t|
+    t.string "genre"
+    t.string "year"
+    t.string "description"
+    t.string "image_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "postal_cards", force: :cascade do |t|
+    t.string "country"
+    t.string "department"
+    t.string "city"
+    t.integer "year"
+    t.string "editor"
+    t.string "description"
+    t.string "image_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
